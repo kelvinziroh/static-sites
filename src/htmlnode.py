41bbde7e5560e27a0fca8b320cb0eb6b8c62159a
Kelvin Ziro <kelvin.ziroh@gmail.com>
2025-12-04 15:16:28 +0300

    Create and test leaf html nodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,4 +1,4 @@
-class HTMLNode():
+class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
@@ -20,4 +20,19 @@ class HTMLNode():
     
     def __repr__(self):
         return f"HTMLNode(tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
     
+    def to_html(self):
+        if not self.value:
+            raise ValueError
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode(tag: {self.tag}, value: {self.value}, props: {self.props})"
+            
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props(self):
@@ -29,6 +29,18 @@ class TestHTMLNode(unittest.TestCase):
             repr(node), 
             "HTMLNode(tag: a, value: You know nothing Jon Snow, children: None, props: {'href': 'https://gotfandom-characters-igrid.com'})"
         )
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Winter is coming")
+        self.assertEqual(node.to_html(), "<p>Winter is coming</p>")
+        
+    def test_leaf_to_html_rtxt(self):
+        node = LeafNode(None, "Hear me roar")
+        self.assertEqual(node.to_html(), "Hear me roar")
+    
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "King of the Andals", {"href": "https://gotfandom/salutation", "target": "_blank"})
+        self.assertEqual(node.to_html(), '<a href="https://gotfandom/salutation" target="_blank">King of the Andals</a>')
 
 
 if __name__ == "__main__":
