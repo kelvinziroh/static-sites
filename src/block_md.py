f79221639940005f3b2b181b365c48bcf4ebbef2
Kelvin Ziro <kelvin.ziroh@gmail.com>
2025-12-14 01:57:27 +0300

    Match block types from input blocks of text

--- src/block_md.py
+++ src/block_md.py
@@ -1,5 +1,30 @@
+import re
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UL = "unordered_list"
+    OL = "ordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = [
         block.strip() for block in markdown.split("\n\n") if block != ""
     ]
     return blocks
+
+def block_to_block_type(block):
+    if re.match(r"^#{1,6}\s.+", block):
+        return BlockType.HEADING
+    elif re.match(r"^`{3}\n?.*\n?`{3}$", block):
+        return BlockType.CODE
+    elif re.match(r">\s", block):
+        return BlockType.QUOTE
+    elif re.match(r"^(-\s.*\n)+", block):
+        return BlockType.UL
+    elif re.match(r"^(\d+\.\s.*\n)+", block):
+        return BlockType.OL
+    else:
+        return BlockType.PARAGRAPH
--- src/test_block_md.py
+++ src/test_block_md.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_md import markdown_to_blocks
+from block_md import *
 
 class TestBlockMD(unittest.TestCase):
     def test_markdown_to_blocks1(self):
@@ -44,7 +44,55 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items"
             ]
         )
-
+    
+    def test_block_to_heading_block(self):
+        blocks = [f"{'#'*i} Don't Panic!" for i in range(1, 7)]
+        block_types = [block_to_block_type(block) for block in blocks]
+        self.assertEqual(
+            block_types,
+            [
+                BlockType.HEADING,
+                BlockType.HEADING,
+                BlockType.HEADING,
+                BlockType.HEADING,
+                BlockType.HEADING,
+                BlockType.HEADING
+            ]
+        )
+        
+    def test_block_to_code_block(self):
+        blocks = [
+            "```This is a string to test code blocks```",
+            "```\nThis is another string to test code blocks as well\n```"
+        ]
+        block_types = [block_to_block_type(block) for block in blocks]
+        self.assertEqual(
+            block_types,
+            [
+                BlockType.CODE,
+                BlockType.CODE
+            ]
+        )
+    
+    def test_block_to_quote_block(self):
+        block = "> This is a block quote!"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+    
+    def test_block_to_ulist_block(self):
+        ulist_block = """- This is a list item
+        - So is this
+        - and this one as well"""
+        block_type = block_to_block_type(ulist_block)
+        self.assertEqual(block_type, BlockType.UL)
+    
+    def test_block_to_olist_block(self):
+        olist_block = """1. This is a list item
+        2. So is this
+        3. and this one as well"""
+        block_type = block_to_block_type(olist_block)
+        self.assertEqual(block_type, BlockType.OL)
+        
 
 if __name__ == "__main__":
     unittest.main()
