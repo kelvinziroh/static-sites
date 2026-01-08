eb325ab286dec3915203a318db5d058b282c9481
Kelvin Ziro <kelvin.ziroh@gmail.com>
2026-01-08 08:31:30 +0300

    Generte more static pages from markdown

--- src/main.py
+++ src/main.py
@@ -17,6 +17,22 @@ def main():
 
     copy_files(src_dir, dest_dir)
     generate_page("content/index.md", "template.html", "public/index.html")
+    generate_page(
+        "content/blog/glorfindel/index.md",
+        "template.html",
+        "public/blog/glorfindel/index.html",
+    )
+    generate_page(
+        "content/blog/majesty/index.md",
+        "template.html",
+        "public/blog/majesty/index.html",
+    )
+    generate_page(
+        "content/blog/tom/index.md", "template.html", "public/blog/tom/index.html"
+    )
+    generate_page(
+        "content/contact/index.md", "template.html", "public/contact/index.html"
+    )
 
 
 if __name__ == "__main__":
