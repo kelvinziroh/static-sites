74397426a07fbb61b8cc356731acd28aeb45c86e
Kelvin Ziro <kelvin.ziroh@gmail.com>
2026-01-09 13:45:10 +0300

    Recursively generate content to the public dir

--- src/generate.py
+++ src/generate.py
@@ -1,6 +1,45 @@
 import os
+from pathlib import Path
 
-from block_md import markdown_to_html_node
+from block_md import markdown_to_blocks, markdown_to_html_node
+
+
+def generate_pages_r(src_path, template_path, dest_path):
+    if not os.path.exists(dest_path):
+        os.mkdir(dest_path)
+
+    for item in os.listdir(src_path):
+        new_src_path = os.path.join(src_path, item)
+
+        if os.path.isfile(new_src_path):
+            file_obj = Path(new_src_path)
+            file_name = file_obj.stem
+            new_dest_path = Path(dest_path) / f"{file_name}.html"
+            generate_page(new_src_path, template_path, new_dest_path)
+        else:
+            new_dest_path = os.path.join(dest_path, item)
+            generate_pages_r(new_src_path, template_path, new_dest_path)
+
+
+def generate_page(src_path, template_path, dest_path):
+    with open(src_path, "r") as file:
+        md = file.read()
+
+    title = extract_title(md)
+    html_content = markdown_to_html_node(md).to_html()
+
+    with open(template_path, "r") as file:
+        template = file.read()
+
+    filled_template = template.replace("{{ Title }}", title)
+    filled_template = template.replace("{{ Content }}", html_content)
+
+    dest_dir = os.path.dirname(dest_path)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+
+    with open(dest_path, "w") as file:
+        file.write(filled_template)
 
 
 def extract_title(markdown):
@@ -8,30 +47,3 @@ def extract_title(markdown):
     if not title.startswith("# "):
         raise Exception("No title present")
     return title.replace("#", "").strip()
-
-
-def generate_page(from_path, template_path, dest_path):
-    print(
-        f'Generating page from "{from_path}" to "{dest_path}" using "{template_path}"'
-    )
-
-    # open and read the markdown file
-    with open(from_path, "r") as file:
-        md = file.read()
-
-    html_content = markdown_to_html_node(md).to_html()
-    title = extract_title(md)
-
-    # Fill in the template
-    with open(template_path, "r") as rd_file:
-        temp = rd_file.read()
-
-    temp = temp.replace("{{ Title }}", title)
-    temp = temp.replace("{{ Content }}", html_content)
-
-    dest_dir = os.path.dirname(dest_path)
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
-
-    with open(dest_path, "w") as wt_file:
-        wt_file.write(temp)
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 from copy_static import copy_files
-from generate import generate_page
+from generate import generate_pages_r
 
 src_dir = "static"
 dest_dir = "public"
@@ -15,24 +15,11 @@ def main():
     if is_dest:
         shutil.rmtree(dest_dir)
 
+    # Recursively copy static files and directories to the public dir
     copy_files(src_dir, dest_dir)
-    generate_page("content/index.md", "template.html", "public/index.html")
-    generate_page(
-        "content/blog/glorfindel/index.md",
-        "template.html",
-        "public/blog/glorfindel/index.html",
-    )
-    generate_page(
-        "content/blog/majesty/index.md",
-        "template.html",
-        "public/blog/majesty/index.html",
-    )
-    generate_page(
-        "content/blog/tom/index.md", "template.html", "public/blog/tom/index.html"
-    )
-    generate_page(
-        "content/contact/index.md", "template.html", "public/contact/index.html"
-    )
+
+    # Recursively generate content to the public dir
+    generate_pages_r("content", "template.html", "public")
 
 
 if __name__ == "__main__":
